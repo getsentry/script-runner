ac8d5cf30db36198fc09d31b62af4324bc6ca1a3
kneeyo1 <neo.huang@sentry.io>
2025-05-08 13:01:07 -0700

    testing bad healthcheck

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,8 +2,9 @@ import os
 from typing import Generator
 
 import pytest
-from flask import Flask, Response
+from flask import Flask, Response, jsonify, make_response
 from flask.testing import FlaskClient
+from pytest import MonkeyPatch
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -50,13 +51,27 @@ def client(
     return app.test_client()
 
 
-def test_health_endpoint(client: "FlaskClient[Response]") -> None:
-    """
-    Test the /health endpoint.
-    """
+def test_good_healthcheck(client: "FlaskClient[Response]") -> None:
     response = client.get("/health")
 
     assert response.status_code == 200
     assert response.content_type == "application/json"
     expected_json = {"status": "ok"}
+    assert response.get_json() == expected_json
+
+
+def test_bad_healthcheck(
+    client: "FlaskClient[Response]", monkeypatch: MonkeyPatch
+) -> None:
+    def mock_bad_health() -> Response:
+        return make_response(
+            jsonify({"status": "error", "reason": "simulated failure"}), 503
+        )
+
+    monkeypatch.setattr("script_runner.app_blueprint.health", mock_bad_health)
+    response = client.get("/health")
+
+    assert response.status_code == 503
+    assert response.content_type == "application/json"
+    expected_json = {"status": "error", "reason": "simulated failure"}
     assert response.get_json() == expected_json
