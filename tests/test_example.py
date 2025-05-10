85682399deef85ce0c505d3a9b00191a28820199
Lyn Nagara <lynnagara@users.noreply.github.com>
2025-05-09 17:33:15 -0700

    test utilities for autocomplete
    now the `execute_with_context` test utility will check a function to see if
    it has any dynamically autocompleted parameters, and generate the
    autocompletions if so.
    
    it asserts that all autocomplete functions return a list of strings,
    and returns the values to the user so that can make test specific
    assertions on them.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,6 @@
 from script_runner.context import FunctionContext
 from script_runner.testutils import execute_with_context
-from tests.example import hello, hello_with_enum
+from tests.example import hello, basic_autocomplete, dynamic_autocomplete
 
 mock_context = FunctionContext(
     region="test",
@@ -9,10 +9,18 @@ mock_context = FunctionContext(
 
 
 def test_simple_function() -> None:
-    result = execute_with_context(hello, mock_context, ["there"])
+    (result, _) = execute_with_context(hello, mock_context, ["there"])
     assert result == "hello there"
 
 
-def test_enum() -> None:
-    result = execute_with_context(hello_with_enum, mock_context, ["bar"])
+def test_basic_autocomplete() -> None:
+    (result, _) = execute_with_context(basic_autocomplete, mock_context, ["bar"])
     assert result == "hello bar"
+
+
+def test_dynamic_autocomplete() -> None:
+    (result, autocompletions) = execute_with_context(
+        dynamic_autocomplete, mock_context, ["foo"]
+    )
+    assert result == "Selected foo"
+    assert autocompletions["value"] == ["0", "1", "2", "3"]
