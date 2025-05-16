f23f62afdd5dff9e573f81ef8d511ca937518272
kneeyo1 <neo.huang@sentry.io>
2025-05-16 14:54:19 -0700

    tests

--- tests/blueprints/test_base_bp.py
+++ tests/blueprints/test_base_bp.py
@@ -1,9 +1,8 @@
 from typing import Generator
 
 import pytest
-from flask import Flask, Response, jsonify, make_response
+from flask import Flask, Response
 from flask.testing import FlaskClient
-from pytest import MonkeyPatch
 
 from script_runner.blueprints.base_app_bp import base_app_bp
 
@@ -20,27 +19,10 @@ def client() -> "Generator[FlaskClient[Response], None, None]":
             yield client
 
 
-def test_good_healthcheck(client: "FlaskClient[Response]") -> None:
+def test_healthcheck(client: "FlaskClient[Response]") -> None:
     response = client.get("/health")
 
     assert response.status_code == 200
     assert response.content_type == "application/json"
     expected_json = {"status": "ok"}
-    assert response.get_json() == expected_json
-
-
-def test_bad_healthcheck(
-    client: "FlaskClient[Response]", monkeypatch: MonkeyPatch
-) -> None:
-    def mock_bad_health() -> Response:
-        return make_response(
-            jsonify({"status": "error", "reason": "simulated failure"}), 503
-        )
-
-    monkeypatch.setattr("script_runner.blueprints.base_app_bp.health", mock_bad_health)
-    response = client.get("/health")
-
-    assert response.status_code == 503
-    assert response.content_type == "application/json"
-    expected_json = {"status": "error", "reason": "simulated failure"}
     assert response.get_json() == expected_json
